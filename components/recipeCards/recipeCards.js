de61dc9856cd36952a16132c03dec20b8c1beea8
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-08-08 09:42:38 +0200

    feat: Add Ellipsis to recipe description
    Text is now cut off after 8 lines of text

--- components/recipeCards/recipeCards.js
+++ components/recipeCards/recipeCards.js
@@ -17,11 +17,11 @@ function showRecipeCards (recipesData) {
                 <div class="card-img-top bg-dark" style="height: 178px ;"></div>
                 <div class="card-body bg-light">
                     <div class="card-title d-flex flex-row justify-content-between">
-                        <h3 class="fs-1">${recipe.name}</h3>
-                        <i class="bi bi-clock fw-bold fst-normal fs-1">&nbsp;&nbsp;${recipe.time}&nbsp;min</i>
+                        <h3 class="fs-1 w-75 pe-1">${recipe.name}</h3>
+                        <i class="bi bi-clock fw-bold fst-normal fs-1 w-25">&nbsp;&nbsp;${recipe.time}&nbsp;min</i>
                     </div>
                     <div class="card-text pt-3 d-flex flex-row justify-content-between">
-                        <ul class="w-50 fs-3 p-0">
+                        <ul class="w-50 pe-2 fs-3 p-0">
                             ${recipeIngredients}
                         </ul>
                         <p class="fs-3 tk-roboto w-50 lh-1">${recipe.description}</p>
